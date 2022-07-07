a943590035bed43594ce27e70cbad5e29e607ca1
VaroWz <antoine.bouille.24@gmail.com>
2022-07-07 23:56:36 +0200

    Update Decuve.c

--- C/Decuve.c
+++ C/Decuve.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int poid;
+static int dose;
+static int heure = 0;
+static double decuve;
+static double alcool;
+static boolean homme;
+
 void main() {
-    printf("Start prgm");
+
+    scanf("Etes vous un homme ? (true/false)", &homme);
+    scanf("Quel est votre poid ? (kg)", &poid);
+    scanf("Combien de verre avez vous bu ? (dose bar)", &dose);
+
+    if(homme==true){
+        alcool = (dose*10)/(poid*0.7);
+        printf(alcool);
+    }
+
+
 }
